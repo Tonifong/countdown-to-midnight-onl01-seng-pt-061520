78ab009368d72fd6acff0f285b3b0dc21b7c3a8b
Antonio O Fong <omarquinzan@yahoo.com>
2020-06-26 00:38:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,21 +2,20 @@
 
 def countdown(number)
   number =10
-  while number < 0
+  while number > 0
   puts "#{number} SECOND(S)!"
   number -=1
-end
+  end
  return "HAPPY NEW YEAR!"
-
- end
+end
  
- countdown(10)
- def countdown_with_sleep(number)
-   while number> 0
-   sleep 1
+countdown(10)
+def countdown_with_sleep(number)
+ while number> 0
+ sleep 1
    puts "should take at least 5 seconds to execute"
    number-=1
- end
+  end
    return "HAPPY NEW YEAR!"
   end
 countdown(10)  
